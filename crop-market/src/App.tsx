427d6bbfd9a1ce185131478abc2368a38f59fcef
tombash766 <87862533+tombash766@users.noreply.github.com>
2025-03-15 14:31:00 +0000

    t

--- crop-market/src/App.tsx
+++ crop-market/src/App.tsx
@@ -162,16 +162,17 @@ import StockDisplay from './components/StockDisplay.jsx';
 
 const App = () => {
   const [seed, setSeed] = useState(0);
+  const [t, setT] = useState(0);
 
   return (
     <div style={{ width: '100vw', height: '100vh', padding:'2em'}}>
       <h1>Stock Market Visualization</h1>
       <div style={{ display: 'flex', flexDirection: "column"}}>
         <div style={{ flex: 1, flexDirection: 'row'}}>
-          <StockDisplay seed={0} value="$1000" />
+          <StockDisplay seed={0} time={t} value="$1000" />
         </div>
         <div style={{ flex: 1, flexDirection: 'row'}}>
-          <StockDisplay seed={1} value="$1050" />
+          <StockDisplay seed={1} time={t} value="$1050" />
         </div>
       </div>
     </div>
