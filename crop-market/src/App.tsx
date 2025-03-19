3ea56e4bbd6bf280e205e62415c75afcfb339572
tombash766 <87862533+tombash766@users.noreply.github.com>
2025-03-19 23:03:17 +0000

    woman II

--- crop-market/src/App.tsx
+++ crop-market/src/App.tsx
@@ -4,16 +4,16 @@ import paper from './pics/paper.jpg';
 import {disaster} from './components/crashes.js'
 import cornico from './pics/cornico.png'
 import wheatico from './pics/wheatico.png'
+import Woman from "./components/woman.jsx"
 
 const App = () => {
+
   let temp = [["none",1,1]]
   const [seed, setSeed] = useState(0);
   const [time, setTime] = useState(0);
   const [disr, setDisr] = useState(temp);
   const [timeTill, setTimeTill] = useState(Math.random()*5);
   const [timeSince, setTimeSince] = useState(0);
-
-  
 
   const probabilities = Array.from({ length: 8 }, () => Math.floor(Math.random() * 100));
   useEffect(() => {
@@ -35,6 +35,7 @@ const App = () => {
 
   return (
     <div>
+      <Woman message={disr[disr.length-1][0]} active={timeSince < 10}/>
       <img src={paper} style={{position: "absolute", width: "100%", height: "100%", zIndex: -1, top: "0px", left: "0px", right: "0px", bottom: "0px"}}></img>
       <h1 style={{justifySelf:"center", margin: "20px"}} >Stock Market Visualization</h1>
       <div style={{ display: 'flex', flexDirection: "column"}}>
--- crop-market/src/components/woman.jsx
+++ crop-market/src/components/woman.jsx
@@ -1,13 +1,15 @@
 import womanImage from "../pics/woman.png"
-import speechBubble from "../pics/speechBubble.png"
+import speechBubble from "../pics/speech.png"
 
-const Woman = ({event, active}) => {
+const Woman = ({message, active}) => {
     return (
         <>
-            <img src={womanImage}></img>
-            <img src={speechBubble}>
-                {event.message}
+            <img src={womanImage} className="woman"></img>
+            <img src={speechBubble} className="speechBubble">
+                {message}
             </img>
         </>
     )
 }
+
+export default Woman
